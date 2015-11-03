33fd7fecc3c652c075a4e1a872d3718714f7b0cf
dnarushevich <d.narushevich@gmail.com>
2015-11-03 15:22:47 +0200

    simple implementation of scopes

---
+++ app/src/common/Scope.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class Scope {
+    static inject(scope, ctor, ...params){
+        var F = function(){};
+        F.prototype = ctor.prototype;
+        var f = new F();
+        f.scope = Scope.spawn(scope);
+        f.constructor = ctor;
+        ctor.apply(f, params);
+
+        return f;
+    }
+
+    static spawn(parentScope = null){
+        return Object.create(parentScope);
+    }
+}
+
+module.exports = Scope;
